e56adc6b36fe7df4677983f7b5d8b73928408917
Robbot <verwolff1@gmail.com>
2017-04-01 10:45:52 +0100

    Rest of weeks

---
+++ W1/src/HelloJohn.java
@@ -0,0 +1,16 @@
+public class HelloJohn {
+	public static void main (String[] args)
+	{
+		System.out.println("H");
+		System.out.println("e");
+		System.out.println("l");
+		System.out.println("l");
+		System.out.println("o");
+		System.out.println(" ");
+		System.out.println("J");
+		System.out.println("o");
+		System.out.println("h");
+		System.out.println("n");
+		System.out.println("!");
+	}
+}
