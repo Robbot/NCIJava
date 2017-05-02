5ca02ed85f63945514a4c8e10e9a12abd8af59b8
Verwolff <verwolff1@gmail.com>
2017-05-02 07:22:15 +0100

    Some study over bank  holiday

---
+++ Mine/src/ReplaceApp.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class ReplaceApp {
+	public static void main (String args[]){
+		String name, newName;
+		Replace myReplace = new Replace();
+		
+		name = JOptionPane.showInputDialog(null,"Enter your name");
+		myReplace.setName(name);
+		
+		myReplace.compute();
+		
+		newName = myReplace.getNewName();
+		JOptionPane.showMessageDialog(null,"The output is "+newName);
+	}
+}
