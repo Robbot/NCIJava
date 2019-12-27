f0711849cbc3d89b8549afd9a8f813fcbf47688c
Robbot <verwolff1@gmail.com>
2019-12-27 08:39:18 +0000

    some rubbish

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+module NCIJava {
+	exports w09;
+	exports ca2;
+	exports w10;
+	exports w02;
+	exports w01;
+	exports w04;
+	exports w03;
+	exports w06;
+	exports com.hello;
+	exports w05;
+	exports w07;
+
+	requires java.desktop;
+	requires java.logging;
+}
